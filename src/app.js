10dd180a7ac6f303a296e885ad9d769f56621cc9
Akshat Chaurasia <akshatchaurasia212005@gmail.com>
2026-06-12 15:51:24 +0530

    Foundational Git and Prettier

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: "true", limit: "16kb" }));
+app.use(express.static("public"));
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN
+      ? process.env.CORS_ORIGIN.split(",")
+      : "https://localhost:7152",
+    credentials: "true",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS "],
+    allowheaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Backend Project Brother");
+});
+
+app.get("/instagram", (req, res) => {
+  res.send("Yaa, Testing Brother");
+});
+
+export default app;
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import app from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const port = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on https://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error: ", error);
+    process.exit(1);
+  });
